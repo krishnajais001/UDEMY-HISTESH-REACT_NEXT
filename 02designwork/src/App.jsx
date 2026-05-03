eb06a027d99b9dd3512716272e93a2265c18f789
Krishna Kumar <krishnajais7091@gmail.com>
2026-05-03 23:50:17 +0530

    first commit

---
+++ 02designwork/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+   <h1 className="flex justify-center  text-blue-900  border-2 p-4 rounded-xl ">
+  Configure Tailwind in React Application
+</h1>
+</>
+   
+  )
+}
+
+export default App
